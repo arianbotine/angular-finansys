62c91936f7a3d62c316f323d099e0613e85ba847
arian <arianbotine@hotmail.com>
2019-04-08 22:50:20 -0300

    Adicionando listagem de categorias

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service";
+import { filter } from 'rxjs/operators';
+import { element } from '@angular/core/src/render3';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[]= [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    );
   }
 
+  deleteCategory(category: Category){
+    const mustDelete = confirm('Deseja realmente excluir este item?')
+
+    if(mustDelete){
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert("Error ao tentar excluir")
+      );
+    }
+  }
+  
 }
